1ffb19f9d4221e27042aaaa8a70b569a111a5719
marko <marko.pavicevic@outlook.com>
2026-04-22 17:19:45 -0700

    initial next.js project setup

--- backend/data/problems.js
+++
@@ -1,9 +0,0 @@
-// mock data base
-const problems = [
-  { id: 1, title: "N-Queens", description: "Lorem ipsum dolor sit amet..." },
-  { id: 2, title: "Down With Brackets", description: "Lorem ipsum dolor sit amet..." },
-  { id: 3, title: "Tower of Hanoi", description: "Lorem ipsum dolor sit amet..." }
-];
-
-module.exports = problems;
-
