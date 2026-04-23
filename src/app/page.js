8108793da4b777dbaaddd0bba021f0eb544ee4db
minjarezjen <154289732+minjarezjen@users.noreply.github.com>
2026-04-22 18:19:41 -0700

    Squashed commit of the following:
    commit affae5280b17cc19c2f553b2d5038319263bc629
    Merge: 4b6c6aa 8f422ba
    Author: marko <marko.pavicevic@outlook.com>
    Date:   Wed Apr 22 18:12:13 2026 -0700
    
        Merge pull request #6 from lucasz26/feature/mock-db
    
        mock DB
    
    commit 8f422ba435f7dccf0f8a66b3e08a493f4fb57a57
    Author: marko <marko.pavicevic@outlook.com>
    Date:   Wed Apr 22 18:09:47 2026 -0700
    
        mock DB
    
    commit 4b6c6aa15dd5fbf3db325e6db43027340141fc0b
    Merge: e736d46 1ffb19f
    Author: marko <marko.pavicevic@outlook.com>
    Date:   Wed Apr 22 17:23:19 2026 -0700
    
        Merge pull request #5 from lucasz26/feature/nextjs-setup
    
        initial next.js project setup
    
    commit 1ffb19f9d4221e27042aaaa8a70b569a111a5719
    Author: marko <marko.pavicevic@outlook.com>
    Date:   Wed Apr 22 17:19:45 2026 -0700
    
        initial next.js project setup

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>CodeBook Home</h1>
+    </main>
+  );
+}
---
+++ src/lib/data.js
@@ -0,0 +1,12 @@
+// mock DB
+// TODO: remove once actual DB is implemented
+
+export const problems = [
+  { id: 1, title: "N-Queens", description: "..." },
+  { id: 2, title: "Two Sum", description: "..." }
+]
+
+export const submissions = [
+  { id: 1, problemId: 1, status: "Accepted", sourceCode: "..." },
+  { id: 2, problemID: 1, status: "Wrong Answer", sourceCode: "..." }
+]
