d834a92c11dd397eb13eff254a0f2b8165979fda
marko <marko.pavicevic@outlook.com>
2026-05-08 23:51:35 -0700

    implement server action to send code to Piston server

--- src/app/solve/[problemId]/actions.js
+++ src/app/solve/[problemId]/actions.js
@@ -13,3 +13,22 @@ export async function saveCode(problemId, code) {
 export async function getResults(submissionId) {
   return await CodebookDBHelpers.getResultsById(submissionId);
 }
+
+export async function runCode(language, code) {
+  try {
+    const response = await fetch("http://localhost:2000/api/v2/execute", {
+      method: "POST",
+      headers: { "Content-Type": "applicatoin/json" },
+      body: JSON.stringify({
+        language: language,
+        version: "*",
+        files: [{ content: code }]
+      })
+    });
+
+    const data = await response.json()
+    return data.run;
+  } catch (error) {
+    return { error: "Piston is unreachable" };
+  }
+}
